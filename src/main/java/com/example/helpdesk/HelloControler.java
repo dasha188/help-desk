aa6282b7dc6391fbe1195db3560f6309859f4d88
Azerty <naumovadashaig@icloud.com>
2026-05-20 20:17:09 +0300

    Initial commit: Help Desk project

--- src/main/java/com/example/helpdesk/HelloControler.java
+++ src/main/java/com/example/helpdesk/HelloControler.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloControler {
 
-   @GetMapping("/")
+   //@GetMapping("/")
     public String hello() {
         return "Hello, Help Desk!";
     }
