2ca7bab521c97dc5b3084c195c5ba15d6d70d57c
Azerty <naumovadashaig@icloud.com>
2026-06-01 12:50:54 +0300

    Module 4: add ticket creation form and service layer

--- src/main/java/com/example/helpdesk/model/Ticket.java
+++ src/main/java/com/example/helpdesk/model/Ticket.java
@@ -56,6 +56,9 @@ public class Ticket {
         }
     }
     public Long getId() { return id; }
+    public void setId(Long id) {
+        this.id = id;
+    }
     public String getTitle() { return title; }
     public void setTitle(String title) { this.title = title; }
 
